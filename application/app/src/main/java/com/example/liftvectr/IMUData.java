8827e11f51c0689a3ad187a12ed3b17d16d777ab
Cole <cole.fisher@ufl.edu>
2022-04-10 21:24:19 -0400

    Working bluetooth connection!!

--- application/app/src/main/java/com/example/liftvectr/IMUData.java
+++ application/app/src/main/java/com/example/liftvectr/IMUData.java
@@ -6,11 +6,11 @@ public class IMUData implements Serializable {
     public float x_lin_acc, y_lin_acc, z_lin_acc;
     public float x_ang_vel, y_ang_vel, z_ang_vel;
     public float x_mag_field, y_mag_field, z_mag_field;
-    public int millisec;
+    public float micros;
 
     public IMUData(float x_a, float y_a, float z_a,
                    float x_g, float y_g, float z_g,
-                   float x_m, float y_m, float z_m, int ms) {
+                   float x_m, float y_m, float z_m, float ms) {
         this.x_lin_acc = x_a;
         this.y_lin_acc = y_a;
         this.z_lin_acc = z_a;
@@ -20,6 +20,20 @@ public class IMUData implements Serializable {
         this.x_mag_field = x_m;
         this.y_mag_field = y_m;
         this.z_mag_field = z_m;
-        this.millisec = ms;
+        this.micros = ms;
+    }
+
+    public IMUData(String[] raw_data) {
+        this.x_lin_acc = Float.parseFloat(raw_data[0]);
+        this.y_lin_acc = Float.parseFloat(raw_data[1]);
+        this.z_lin_acc = Float.parseFloat(raw_data[2]);
+        this.x_ang_vel = Float.parseFloat(raw_data[3]);
+        this.y_ang_vel = Float.parseFloat(raw_data[4]);
+        this.z_ang_vel = Float.parseFloat(raw_data[5]);
+        this.x_mag_field = Float.parseFloat(raw_data[6]);
+        this.y_mag_field = Float.parseFloat(raw_data[7]);
+        this.z_mag_field = Float.parseFloat(raw_data[8]);
+        this.micros = Float.parseFloat(raw_data[9]);
+
     }
 }
--- application/app/src/main/java/com/example/liftvectr/MainActivity.java
+++ application/app/src/main/java/com/example/liftvectr/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.liftvectr;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothProfile;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.MacAddress;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ederdoski.simpleble.interfaces.BleCallback;
+import com.ederdoski.simpleble.models.BluetoothLE;
+import com.ederdoski.simpleble.utils.BluetoothLEHelper;
+import com.ederdoski.simpleble.utils.Constants;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +42,22 @@ public class MainActivity extends AppCompatActivity {
     private Spinner exerciseSpinner;
     private TextView x_accel, y_accel, z_accel;
     private TextView x_gyro, y_gyro, z_gyro;
+    private TextView bluetoothConnected;
 
     private Exercise newExercise;
 
+    private final String SERVICE_UUID = "0000181C-0000-1000-8000-00805f9b34fb";
+    private final String CHAR_UUID = "00002ADA-0000-1000-8000-00805f9b34fb";
+
+    BluetoothLEHelper ble;
+   // private MacAddress MAC_ADDRESS = new MacAddress("1C:35:7E:C5:F9:3B");
     private boolean exerciseOngoing = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         exerciseBtn = (Button) findViewById(R.id.button);
         viewChartBtn = (Button) findViewById(R.id.view_chart_button);
@@ -36,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
         x_gyro = (TextView) findViewById(R.id.x_g);
         y_gyro = (TextView) findViewById(R.id.y_g);
         z_gyro = (TextView) findViewById(R.id.z_g);
+        bluetoothConnected = (TextView) findViewById(R.id.bluetooth_status);
 
         exerciseSpinner = (Spinner) findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -45,6 +77,38 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         exerciseSpinner.setAdapter(adapter);
+
+        bluetoothConnected.setText("Not Connected");
+        // ============ BLUETOOTH =============
+        ble = new BluetoothLEHelper(this);
+        boolean scanFinished = false;
+
+
+        //while (!deviceFound) {
+            //System.out.println("WAITING FOR DEVICE");
+        askForPermissions(this);
+            if (ble.isReadyForScan()) {
+                    Handler mHandler = new Handler();
+                    ble.scanLeDevice(true);
+
+//                  System.out.println("Scan period: " + ble.getScanPeriod());
+//
+                    mHandler.postDelayed(() -> {
+                        //--The scan is over, you should recover the found devices.
+                        Log.v("Devices found: ", String.valueOf(ble.getListDevices()));
+                        findMatchingDevice();
+                    }, ble.getScanPeriod());
+
+                    //findMatchingDevice();
+
+            }
+            else {
+                System.out.println("PERMISSIONS!!!");
+            }
+
+
+
+        //}
 
         exerciseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,19 +122,29 @@ public class MainActivity extends AppCompatActivity {
                     newExercise = new Exercise(exerciseSpinner.getSelectedItem().toString(), Calendar.getInstance().getTime());
 
                     // Fill exercise with fake bluetooth data
-                    newExercise.addDataSample(new IMUData(0.2f, 1.0f, 0.43f, 0.0f, 0.1f, 0.0f, -9.0f, -1.0f, -1.0f, 1));
-                    newExercise.addDataSample(new IMUData(0.23f, 0.95f, 0.41f, 0.3f, 0.2f, 0.3f, -8.4f,-1.0f, -0.8f, 2));
-                    newExercise.addDataSample(new IMUData(0.28f, 1.10f, 0.39f, 0.5f, 0.1f, 0.5f, -9.3f,-0.9f, -1.0f, 3));
-                    newExercise.addDataSample(new IMUData(0.25f, 1.03f, 0.43f, 0.3f, 0.3f, 0.9f, -7.2f,-1.0f, -0.9f, 4));
-                    newExercise.addDataSample(new IMUData(0.29f, 0.93f, 0.45f, 0.0f, 0.2f, 1.1f, -6.3f,-0.9f, -1.0f, 5));
-                    newExercise.addDataSample(new IMUData(0.24f, 0.98f, 0.49f, 0.3f, 0.1f, 3.3f, -6.8f,-1.0f, -1.3f, 6));
-                    newExercise.addDataSample(new IMUData(0.22f, 1.01f, 0.46f, 0.5f, 0.0f, 3.0f, -9.5f,-0.8f, -1.0f, 7));
-                    newExercise.addDataSample(new IMUData(0.21f, 1.03f, 0.42f, 0.3f, 0.0f, 2.1f, -10.0f,-1.0f, -1.2f, 8));
-                    newExercise.addDataSample(new IMUData(0.24f, 0.94f, 0.40f, 0.0f, 0.1f, 1.2f, -9.1f, -0.9f, -1.0f, 9));
-                    newExercise.addDataSample(new IMUData(0.26f, 0.99f, 0.43f, 0.3f, 0.2f, 0.4f, -9.3f,-1.2f, -1.1f, 10));
+//                    newExercise.addDataSample(new IMUData(0.2f, 1.0f, 0.43f, 0.0f, 0.1f, 0.0f, -9.0f, -1.0f, -1.0f, 1));
+//                    newExercise.addDataSample(new IMUData(0.23f, 0.95f, 0.41f, 0.3f, 0.2f, 0.3f, -8.4f,-1.0f, -0.8f, 2));
+//                    newExercise.addDataSample(new IMUData(0.28f, 1.10f, 0.39f, 0.5f, 0.1f, 0.5f, -9.3f,-0.9f, -1.0f, 3));
+//                    newExercise.addDataSample(new IMUData(0.25f, 1.03f, 0.43f, 0.3f, 0.3f, 0.9f, -7.2f,-1.0f, -0.9f, 4));
+//                    newExercise.addDataSample(new IMUData(0.29f, 0.93f, 0.45f, 0.0f, 0.2f, 1.1f, -6.3f,-0.9f, -1.0f, 5));
+//                    newExercise.addDataSample(new IMUData(0.24f, 0.98f, 0.49f, 0.3f, 0.1f, 3.3f, -6.8f,-1.0f, -1.3f, 6));
+//                    newExercise.addDataSample(new IMUData(0.22f, 1.01f, 0.46f, 0.5f, 0.0f, 3.0f, -9.5f,-0.8f, -1.0f, 7));
+//                    newExercise.addDataSample(new IMUData(0.21f, 1.03f, 0.42f, 0.3f, 0.0f, 2.1f, -10.0f,-1.0f, -1.2f, 8));
+//                    newExercise.addDataSample(new IMUData(0.24f, 0.94f, 0.40f, 0.0f, 0.1f, 1.2f, -9.1f, -0.9f, -1.0f, 9));
+//                    newExercise.addDataSample(new IMUData(0.26f, 0.99f, 0.43f, 0.3f, 0.2f, 0.4f, -9.3f,-1.2f, -1.1f, 10));
+
+                    if (ble.isConnected()) {
+                        System.out.println("READING!!!");
+                        for(int i = 0; i < 1000000; i++) {
+                            ble.read(SERVICE_UUID, CHAR_UUID);
+                        }
+                    }
+                    else {
+                        System.out.println("BLUETOOTH NOT CONNECTED");
+                    }
 
                     // We'd display live data within the UI's table using this function
-                    displayData(new IMUData(1.215f, 3.983f, 0.015f, 3.947f, 5.543f, 0.132f, -1.0f, -3.2f, -9.3f, 1));
+                    //displayData(new IMUData(1.215f, 3.983f, 0.015f, 3.947f, 5.543f, 0.132f, -1.0f, -3.2f, -9.3f, 1));
 
                 }
                 else {
@@ -91,6 +165,26 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void findMatchingDevice() {
+        System.out.println("+++++++MATCHING CODE BEGIN++++++");
+        if (!ble.getListDevices().isEmpty()) {
+            for (BluetoothLE item : ble.getListDevices()) {
+                if(item != null && item.getName() != null) {
+                    System.out.println(item.getName().toString());
+                    System.out.println(item.getName().substring(0, 7));
+                    if (item.getName().substring(0, 7).equals("IMUData")) {
+                        System.out.println("DEVICE FOUND!!");
+                        BluetoothDevice device = item.getDevice();
+                        ble.connect(device, bleCallbacks());
+                        bluetoothConnected.setText("Connected");
+                        System.out.println("DEVICE CONNECTED!!");
+                    }
+                }
+            }
+        }
+        System.out.println("+++++++MATCHING CODE END++++++");
+    }
+
     public void transitionToChartDisplayActivity(String config)
     {
         Intent intent = new Intent(this, ChartDisplay.class);
@@ -106,5 +200,130 @@ public class MainActivity extends AppCompatActivity {
         x_gyro.setText(Float.toString(sample.x_ang_vel));
         y_gyro.setText(Float.toString(sample.y_ang_vel));
         z_gyro.setText(Float.toString(sample.z_ang_vel));
+    }
+
+    @Override
+    protected void onDestroy() {
+        ble.disconnect();
+        super.onDestroy();
+    }
+
+    private BleCallback bleCallbacks(){
+        return new BleCallback(){
+
+            @Override
+            public void onBleConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+                super.onBleConnectionStateChange(gatt, status, newState);
+
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    runOnUiThread(() -> Toast.makeText(MainActivity.this, "Connected to GATT server.", Toast.LENGTH_SHORT).show());
+                }
+
+                if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                    runOnUiThread(() -> Toast.makeText(MainActivity.this, "Disconnected from GATT server.", Toast.LENGTH_SHORT).show());
+                }
+            }
+
+            @Override
+            public void onBleServiceDiscovered(BluetoothGatt gatt, int status) {
+                super.onBleServiceDiscovered(gatt, status);
+                if (status != BluetoothGatt.GATT_SUCCESS) {
+                    Log.e("Ble ServiceDiscovered","onServicesDiscovered received: " + status);
+                }
+            }
+
+            @Override
+            public void onBleCharacteristicChange(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+                super.onBleCharacteristicChange(gatt, characteristic);
+                Log.i("BluetoothLEHelper","onCharacteristicChanged Value: " + Arrays.toString(characteristic.getValue()));
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onBleRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                super.onBleRead(gatt, characteristic, status);
+
+                if (status == BluetoothGatt.GATT_SUCCESS) {
+                    byte[] raw_data = characteristic.getValue();
+                    //Log.i("TAG", Arrays.toString(raw_data));
+                    String decoded = new String(raw_data);
+                    //System.out.println("Decoded: " + decoded);
+
+                    String[] values = decoded.split(",");
+                    boolean valid = true;
+                    for (int i = 0; i < values.length; i++) {
+                        if(values[i].chars().filter(ch -> ch == '.').count() != 1) {
+                            valid = false;
+                        }
+                    }
+                    //System.out.println("Split: " + values.toString());
+
+                    if (values != null && values.length == 10 && valid) {
+                        IMUData parsed_data = new IMUData(values);
+                        newExercise.addDataSample(parsed_data);
+                        displayData(parsed_data);
+                    }
+
+                    runOnUiThread(() -> Toast.makeText(MainActivity.this, "onCharacteristicRead : "+Arrays.toString(characteristic.getValue()),             Toast.LENGTH_SHORT).show());
+                }
+            }
+
+            @Override
+            public void onBleWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                super.onBleWrite(gatt, characteristic, status);
+                runOnUiThread(() -> Toast.makeText(MainActivity.this, "onCharacteristicWrite Status : " + status, Toast.LENGTH_SHORT).show());
+            }
+        };
+    }
+
+    public static void askForPermissions(Activity activity) {
+        List<String> permissionsToAsk = new ArrayList<>();
+        int requestResult = 0;
+
+        if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // Ask for permission
+            permissionsToAsk.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_BACKGROUND_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // Ask for permission
+            permissionsToAsk.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION);
+        }
+
+        if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // Ask for permission
+            permissionsToAsk.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+
+        if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // Ask for permission
+            permissionsToAsk.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.BLUETOOTH) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // Ask for permission
+            permissionsToAsk.add(Manifest.permission.BLUETOOTH);
+        }
+        if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.BLUETOOTH_ADMIN) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // Ask for permission
+            permissionsToAsk.add(Manifest.permission.BLUETOOTH_ADMIN);
+        }
+        if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.BLUETOOTH_PRIVILEGED) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // Ask for permission
+            permissionsToAsk.add(Manifest.permission.BLUETOOTH_PRIVILEGED);
+        }
+
+        if (permissionsToAsk.size() > 0) {
+            ActivityCompat.requestPermissions(activity, permissionsToAsk.toArray(new String[permissionsToAsk.size()]), requestResult);
+        }
+
+        System.out.println("Permissions function has been called");
     }
 }
