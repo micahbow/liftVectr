61885df6d111c1bfc9ad45c2d5ebb11c17bd5841
TimothyKrause20 <timothykrause@ufl.edu>
2022-09-20 19:53:32 -0400

    Cropping based on selected position of raw data chart

--- application/app/src/main/java/com/example/liftvectr/activities/CropExerciseActivity.java
+++ application/app/src/main/java/com/example/liftvectr/activities/CropExerciseActivity.java
@@ -7,22 +7,37 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.liftvectr.R;
 import com.example.liftvectr.data.Exercise;
+import com.example.liftvectr.data.IMUData;
 import com.example.liftvectr.database.ExerciseViewModel;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.highlight.Highlight;
+
+import java.util.ArrayList;
 
 public class CropExerciseActivity extends AppCompatActivity {
 
     private LineChart IMULineChart;
     private Button cancelButton;
     private Button saveButton;
+    private Button cropButton;
 
     private ExerciseViewModel exerciseViewModel;
-    private Exercise adjustedExercise;
+    private Exercise exercise;
+
+    private Highlight[] highlighted;
+    private float highlightedX;
+    private float highlightedY;
+
+    private ArrayList<IMUData> exerciseData;
+    private ArrayList<IMUData> adjustedExerciseData;
+
+    // private HighLight highlighted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +52,22 @@ public class CropExerciseActivity extends AppCompatActivity {
 
         // Accessing the exercise/config data sent over from ExerciseHistoryActivity
         Intent intent = getIntent();
-        Exercise exercise = (Exercise) intent.getSerializableExtra("exercise");
+        exercise = (Exercise) intent.getSerializableExtra("exercise");
 
         displayIMUDataChart(exercise, IMULineChart, "Recorded IMU Data");
 
         // User needs to be able to adjust the exercise passed in from the intent
-        adjustedExercise = exercise;
+        IMULineChart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                highlighted = IMULineChart.getHighlighted();
+                if(highlighted != null)
+                {
+                    cropExercise();
+                }
+            }
+        });
+
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +79,7 @@ public class CropExerciseActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                exerciseViewModel.saveExercise(adjustedExercise);
+                exerciseViewModel.saveExercise(exercise);
                 transitionToExerciseHistoryActivity();
             }
         });
@@ -70,6 +95,19 @@ public class CropExerciseActivity extends AppCompatActivity {
     {
         Intent intent = new Intent(this, ExerciseHistoryActivity.class);
         startActivity(intent);
+    }
+
+    public void cropExercise()
+    {
+            highlightedX = highlighted[0].getX();
+            highlightedY = highlighted[0].getY();
+            exerciseData = exercise.getData();
+            adjustedExerciseData = new ArrayList<>();
+            for(int i = 0; i < (int) highlightedX; i++)
+            {
+                adjustedExerciseData.add(exerciseData.get(i));
+            }
+            exercise.setData(adjustedExerciseData);
     }
 
 }
