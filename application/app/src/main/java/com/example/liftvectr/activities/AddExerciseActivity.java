f24c62396700384797c4974435fa96607edcb362
Cole <cole.fisher@ufl.edu>
2022-09-23 12:38:26 -0400

    emulatioMode = false

--- application/app/src/main/java/com/example/liftvectr/activities/AddExerciseActivity.java
+++ application/app/src/main/java/com/example/liftvectr/activities/AddExerciseActivity.java
@@ -52,7 +52,7 @@ public class AddExerciseActivity extends AppCompatActivity {
 
     // For emulating ONLY: MODIFY this to true to allow start/stop exercise to be pressed, creating
     // a fake exercise and transitioning to CropExerciseActivity during emulation
-    public static boolean emulationMode = true;
+    public static boolean emulationMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
