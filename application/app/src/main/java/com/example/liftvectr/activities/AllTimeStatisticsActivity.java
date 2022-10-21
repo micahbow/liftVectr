19fe2a9999930ace8bfd35148ffe1fa0d2f192f0
TimothyKrause20 <timothykrause@ufl.edu>
2022-10-21 13:21:31 -0400

    Average Force over Time ready for merging

--- application/app/src/main/java/com/example/liftvectr/activities/AllTimeStatisticsActivity.java
+++ application/app/src/main/java/com/example/liftvectr/activities/AllTimeStatisticsActivity.java
@@ -1,5 +1,7 @@
 package com.example.liftvectr.activities;
 
+import static com.example.liftvectr.util.ChartDisplay.displaySingleLineChart;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
@@ -12,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,16 +25,24 @@ import com.example.liftvectr.data.Exercise;
 import com.example.liftvectr.database.ExerciseViewModel;
 import com.example.liftvectr.data.IMUData;
 import com.example.liftvectr.util.ReadRunnable;
+import com.github.mikephil.charting.charts.LineChart;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Date;
 
 public class AllTimeStatisticsActivity extends AppCompatActivity {
     private Spinner exerciseSpinner;
     private EditText weightInput;
     private Button exerciseBtn;
+
+    private ExerciseViewModel exerciseViewModel;
+    private ListView exerciseList;
+    private List<Exercise> savedExercises;
+
+    private LineChart avgForceTimeChart;
 
 
     @Override
@@ -68,6 +79,7 @@ public class AllTimeStatisticsActivity extends AppCompatActivity {
         exerciseSpinner = (Spinner) findViewById(R.id.spinner);
         weightInput = (EditText) findViewById(R.id.editWeight);
         exerciseBtn = (Button) findViewById(R.id.button);
+//        avgForceTimeChart = (LineChart) findViewById(R.id.avgForceTime);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -77,6 +89,12 @@ public class AllTimeStatisticsActivity extends AppCompatActivity {
         // Apply the adapter to the spinners
 
         exerciseSpinner.setAdapter(adapter);
+
+        // Populating Exercises
+        exerciseViewModel = new ViewModelProvider(this).get(ExerciseViewModel.class);
+        exerciseViewModel.getAllExercises().observe(this, exercises -> {
+            savedExercises = exerciseViewModel.getAllExercises().getValue();
+        });
 
 
         exerciseBtn.setOnClickListener(new View.OnClickListener() {
@@ -95,10 +113,50 @@ public class AllTimeStatisticsActivity extends AppCompatActivity {
                         return;
                     }
 
-                    String exercise = exerciseSpinner.getSelectedItem().toString();
+                    String exerciseType = exerciseSpinner.getSelectedItem().toString();
 
-                    Log.i("weight: ", String.valueOf(weight));
-                    Log.i("exercise: ", exercise);
+                    // If database contains saved exercises, look for any matching input criteria
+                    if(!savedExercises.isEmpty()) {
+                        int numMatches = 0;
+                        List<Exercise> exerciseMatches = new ArrayList<Exercise>();
+                        List<Float> avgForces = new ArrayList<Float>();
+                        List<Date> dates = new ArrayList<Date>();
+
+                        List<Float> tempPlotting = new ArrayList<Float>();
+
+                        for(int i = 0; i < savedExercises.size(); i++) {
+                            if(savedExercises.get(i).getType().equals(exerciseType) &&
+                                    savedExercises.get(i).getWeight() == weight) {
+                                exerciseMatches.add(savedExercises.get(i));
+                                numMatches++;
+                            }
+                        }
+
+                        if(numMatches != 0) {
+                            for(int i = exerciseMatches.size()-1; i >= 0; i--) {
+                                Float force = new Float(exerciseMatches.get(i).getAvgForce());
+                                avgForces.add(force);
+                                dates.add(exerciseMatches.get(i).getDate());
+                            }
+
+                            for(int i = 0; i < exerciseMatches.size(); i++) {
+                                Float temp = new Float(i);
+                                tempPlotting.add(temp);
+                            }
+
+                            avgForceTimeChart = (LineChart) findViewById(R.id.avgForceTime);
+
+                            displaySingleLineChart(avgForceTimeChart,
+                                    tempPlotting,
+                                    avgForces,
+                                    "Date", "Average Force");
+                        }
+                        else {
+                            setToastText("No exercises matching this criteria were found!");
+                            return;
+                        }
+
+                    }
                 }
             }
         });
